d320d3a98d078f7121d951ad1901249fba1fbdc2
Shan Wijenayaka <shanwijenayaka@outlook.com>
2020-06-04 00:29:18 +0530

    add function to crash on unhandled error (db)

--- config/db.js
+++ config/db.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
 const connectMongo = async () => {
-    try {
-        const mongoConn = await mongoose.connect(process.env.DB_MONGO_URI, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-        });
-        console.log('mongodb connected', mongoConn.connection.host);
-    } catch (err) {
-        console.log('db connection error', err);
-    }
+    const mongoConn = await mongoose.connect(process.env.DB_MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    });
+    console.log('mongodb connected', mongoConn.connection.host);
 };
 
 module.exports = connectMongo;
